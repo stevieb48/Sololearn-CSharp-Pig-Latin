e61635487db7ba9729f4fc470a9d8ff5490b7f35
stevieb48 <seb48@students.uwf.edu>
2023-03-08 17:32:12 -0600

    built pig latin converter class and completed testing

--- Sololearn Pig Latin/Program.cs
+++ Sololearn Pig Latin/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Text;
 
 /*
 
@@ -33,17 +35,111 @@ namespace Sololearn_Pig_Latin
     {
         static void Main(string[] args)
         {
-            //comment out before Sololearn submission
+            //comment out for Sololearn submission
             // test input for visual studio
-            string sentence = "This is a pig latin sentence converter";
+            string sentence = "This is a pig latin converter";
 
-            //comment out before testing visual studio
+            //comment out before for visual studio
             // get the input from Sololearn
             //string sentence = Console.ReadLine();
 
-            //comment out before Sololearn submission
+            // New pig latin converter object
+            PigLatinSentenceConverter sentenceconverter = new PigLatinSentenceConverter(sentence);
+
+            // send cards to be ranked and output tanking
+            Console.WriteLine(sentenceconverter.OutputSentence());
+
+            //comment out for Sololearn submission
             // hold prompt for testing
             Console.ReadKey();
+        }
+
+        // this class contains all the properties and functions to handle and evaluate the pig lating sentence converter
+        class PigLatinSentenceConverter
+        {
+            // CONSTANTS
+            private const string ADD_ON = "ay";
+            private const int firstCharacter = 0;
+            private const int secondCharacter = 1;
+
+            // array to split the input on a space
+            private string[] oldSentence;
+
+            // variable to store word pulled from sentence array
+            private string newWord = "";
+
+            // variable to store front character pulled from temp word
+            private string tempLetter = "";
+
+            // to build new sentence
+            private ArrayList newSentence = new ArrayList();
+
+            // constructor
+            public PigLatinSentenceConverter(string sentence)
+            {
+                // seperate the input sentence
+                oldSentence = sentence.Split(' ');
+
+                // change the words
+                ChangeWords();
+
+                // make a new sentence
+                BuildNewSentence();
+            }
+
+            // method to change the words
+            private void ChangeWords()
+            {             
+                // every word in sentence
+                foreach (string word in oldSentence)
+                {
+                    // reset temp word
+                    newWord = "";
+
+                    // store first character
+                    tempLetter = word[firstCharacter].ToString();
+
+                    // loop through letters starting with second character
+                    for (int i = secondCharacter; i < word.Length; i++)
+                    {
+                        // add to current tempword with next letter
+                        newWord += word[i].ToString();
+                    }
+
+                    // last pass dont add the space
+                    newWord += tempLetter + ADD_ON;
+
+                    //build the new sentence
+                    BuildNewSentence();
+                }
+            }
+
+            // method to build the new sentence
+            private void BuildNewSentence()
+            {
+                // add temp word to new sentence
+                newSentence.Add(newWord + " ");
+            }
+
+            // output the new sentence from array list
+            public string OutputSentence()
+            {
+                // make new string builder object
+                StringBuilder s = new StringBuilder();
+
+                // remove extra word
+                newSentence.RemoveAt(oldSentence.Length - 1);
+
+                // loop through sentence array list
+                foreach (string word in newSentence)
+                {
+                    // append the word
+                    s.Append(word);
+                }
+
+                // return the new string
+                return s.ToString();
+            }
         }
     }
 }
